794093dd27879ba8922cc6709c0e2b4100da6e82
Lola Amelia <amelialola465@gmail.com>
2024-02-01 11:16:23 +0700

    merapikan baris kode

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import json
 import Counter
 import time
 from servo import Servo
-import servo
 import lcd_display
 import waterpump
-# import buzzer
+import buzzer
 import cProfile
 
 servo = Servo()
@@ -27,41 +26,34 @@ while(True):
             harga_value = int(response_json['harga'])
             lcd_display.display(jumlah_value=jumlah_value,harga_value=harga_value*jumlah_value)
 
-            # #buka servo
-            servo.open()
+            servo.open()                # Hidupkan servo 
+            waterpump.on()              # Hidupkan waterpump
             
-            # hidupkan waterpump
-            waterpump.on()
-            
-            # call hitung
-            Counter.count(jumlah_value)
+            Counter.count(jumlah_value) # call hitung
+           
+            servo.close()             # Matikan servo
+            waterpump.off()           # Matikan waterpump
+            buzzer.hidup()            # Bunyikan buzzer
 
-            # tutup servo dan bunyi buzzer
-            servo.close()
-            # buzzer.hidup()
-            # print("beep")
-
-            waterpump.off()
-
-            # tampilkan data di lcd
-            print(jumlah_value*harga_value)
-
+            print(jumlah_value*harga_value) # Print harga total di terminal
 
             # # reset table status
             # x = requests.post('https://fishcounterta.000webhostapp.com/status.php') 
             # print(x.json())
+
     except KeyboardInterrupt:
         waterpump.off()
         servo.close()
         lcd_display.close()
         break
+
     except Exception as e:
         waterpump.off()
         servo.close()
         lcd_display.close()
         print(e.args)
     #     print(traceback.format)
+        
     finally:
-        # print(jumlah_value)
         time.sleep(3)
 
